fd22dbe172c2b708b17255403e06fe88446beb26
Subikshasa <Subikshasa@example.com>
2024-07-09 10:32:55 +0530

    Java basic program

--- Java_Project1/src/Day3/Student.java
+++ Java_Project1/src/Day3/Student.java
@@ -7,27 +7,23 @@ public class Student {
 	public int getSid() {
 		return sid;
 	}
+
+
 	public void setSid(int sid) {
 		this.sid = sid;
 	}
-	public String getSname() {
-		return sname;
-	}
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
-	public String getScity() {
-		return city;
-	}
-	public void setScity(String scity) {
-		this.city = city;
-	}
+
+
 	public String getCity() {
 		return city;
 	}
+
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
+
 	@Override
 	public String toString() {
 		return "Student [sid=" + sid + ", sname=" + sname + ", city=" + city + "]";
